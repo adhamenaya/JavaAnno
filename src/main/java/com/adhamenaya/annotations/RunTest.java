6969a02e79d30a001f3711aa24f91ff2c0bdf2bf
ENAYA <AENAYA@unrwa.org>
2016-07-09 23:59:19 +0300

    initial commit

---
+++ src/main/java/com/adhamenaya/annotations/RunTest.java
@@ -0,0 +1,7 @@
+package com.adhamenaya.annotations;
+public class RunTest{
+	
+	public static void main(String args[]){
+		System.out.println("Hi Adham");
+	}
+}
---
+++ src/main/java/com/adhamenaya/annotations/TesterInfo.java
@@ -0,0 +1,20 @@
+package com.adhamenaya.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE) // class level
+public @interface TesterInfo{
+
+	public enum Priority {
+		LOW, MEDIUM, HIGH
+	}
+	
+	public Priority proirity() default Priority.HIGH;
+	public String createdBy() default "Adham Enaya";
+	public String[] tags() default "";
+	public String lastModified() default "09.07.2016";
+}
